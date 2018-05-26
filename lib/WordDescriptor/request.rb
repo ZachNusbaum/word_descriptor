53063d6f81d654012a67f4860510cb64d55a6623
Zach Nusbaum <zachdnusbaum@gmail.com>
2018-05-25 20:17:18 -0700

    add new method with_meaning_like(meaning);

--- lib/WordDescriptor/request.rb
+++ lib/WordDescriptor/request.rb
@@ -4,10 +4,15 @@ class WordDescriptor::Request
   include UpDupable
 
   def initialize(rel_jjb: nil, max_count: nil, limit: nil,
-                rel_trg: nil)
+                rel_trg: nil, ml: nil)
     @rel_jjb = rel_jjb
     @rel_trg = rel_trg
     @max_count = max_count
+    @ml = ml
+  end
+
+  def with_meaning_like(meaning)
+    dup_and_update(ml: meaning)
   end
 
   def that_describe(noun)
@@ -27,6 +32,10 @@ class WordDescriptor::Request
     response.parsed_response
   end
 
+  def filtered_tags(tags, regex)
+    tags.select{ |t| t =~ /^(n|v|adj|adv|u)$/ }.join(', ')
+  end
+
   private
 
   def path
@@ -38,8 +47,10 @@ class WordDescriptor::Request
     p += rel_jjb_param if @rel_jjb
     p += max_count_param if @max_count
     p += rel_trg_param if @rel_trg
+    p += ml_param if @ml
     p += metadata_param
     p += ipa_param
+    p += vocab_param
   end
 
   def rel_jjb_param
@@ -54,11 +65,19 @@ class WordDescriptor::Request
     "&rel_trg=#{@rel_trg.gsub(/\s+/, '+')}" if @rel_trg
   end
 
+  def ml_param
+    "&ml=#{@ml.gsub(/\s+/, '+')}" if @ml
+  end
+
   def metadata_param
     "&md=dpsrf"
   end
 
   def ipa_param
     "&ipa=1"
+  end
+
+  def vocab_param
+    "&v=enwiki"
   end
 end
